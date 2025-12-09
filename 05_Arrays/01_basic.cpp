fd2f5ead61f3c43700c95283909226efc6f7d024
MrPkMehta <priyamanshu143@gmail.com>
2025-12-09 23:55:18 +0530

    basic array

---
+++ 05_Arrays/01_basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[50] = {80, 65, 76};
+
+    cout << marks[0] << endl;
+    cout << marks[1] << endl; 
+    cout << marks[2] << endl; 
+    cout << marks[49] << endl; // gives 0 no any value is stored
+
+
+    return 0;
+}
